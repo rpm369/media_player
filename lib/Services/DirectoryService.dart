2d8dac67de8399f41d4820daab3a7190977f77cf
rpm369 <963rpm@gmail.com>
2026-06-29 13:57:01 +0530

    Implemented the DirectoryService methods

--- lib/Services/DirectoryService.dart
+++ lib/Services/DirectoryService.dart
@@ -1,15 +1,53 @@
+import 'dart:io';
+
 import 'package:media_player/DomainModels/AppDirectory.dart';
 import 'package:media_player/Repos/DirectoryRepo.dart';
+import 'package:path/path.dart' as p;
 
 class DirectoryService {
   final DirectoryRepository _repo;
   DirectoryService({required this._repo});
 
-  Future<void> addToFavorite({required AppDirectory dir}) async {}
-  Future<void> removeFromFavorite({required AppDirectory dir}) async {}
-  Future<void> deleteDirectory({required AppDirectory dir}) async {}
+  Future<List<AppDirectory>> getAllFavoriteDirectory() async {
+    List<AppDirectory> allFavDir = await _repo.getAllFavoriteDirectory();
+    List<AppDirectory> validDirs = [];
+    for (AppDirectory dir in allFavDir) {
+      bool existInDisk = await Directory(dir.directoryPath).exists();
+      if (!existInDisk) {
+        await removeFromFavorite(dir: dir);
+      } else {
+        validDirs.add(dir);
+      }
+    }
+    return validDirs;
+  }
+
+  Future<int> addToFavorite({required AppDirectory dir}) async {
+    return await _repo.addToFavorite(dir: dir);
+  }
+
+  Future<void> removeFromFavorite({required AppDirectory dir}) async {
+    await _repo.removeFromFavorite(id: dir.id!);
+  }
+
+  Future<void> deleteDirectory({required AppDirectory dir}) async {
+    if (dir.id != null) await removeFromFavorite(dir: dir);
+    await Directory(dir.directoryPath).delete(recursive: true);
+  }
+
   Future<void> renameDirectory({
     required AppDirectory dir,
     required String newName,
-  }) async {}
+  }) async {
+    Directory directory = Directory(dir.directoryPath);
+    String currentPath = p.dirname(directory.path);
+
+    String newPath = p.join(currentPath, newName);
+    await directory.rename(newPath);
+
+    if (dir.id != null) {
+      await removeFromFavorite(dir: dir);
+      await addToFavorite(dir: AppDirectory(directoryPath: newPath));
+    }
+  }
 }
